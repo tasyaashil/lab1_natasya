64c41cb1eb807f9e9910a50920e456c35700e80f
natasyaashil <natasya.ashil@ui.ac.id>
2022-09-15 14:30:40 +0700

    tutorial 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from wishlist.views import show_wishlist
-from wishlist.views import show_wishlist #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import show_wishlistxml, show_wishlistjson, show_wishlistxmljson
+
 app_name = 'wishlist'
 
 urlpatterns = [
-    path('', show_wishlist, name='show_wishlist'),
-    # path('xml/', show_wishlist, name='show_wishlist'), #sesuaikan dengan nama fungsi yang dibuat
-    # path('json/', show_wishlist, name='show_wishlist'), #sesuaikan dengan nama fungsi yang dibuat
-    path('json/<int:id>', show_wishlist, name='show_wishlist'), #sesuaikan dengan nama fungsi yang dibuat
+    # path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_wishlistxml, name='show_wishlistxml'), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/', show_wishlistjson, name='show_wishlistjson'), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/<int:id>', show_wishlistxmljson, name='show_wishlistxmljson'), #sesuaikan dengan nama fungsi yang dibuat
 
 
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -6,16 +6,28 @@ from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
 # Create your views here.
-def show_wishlist(request):
-    # data = BarangWishlist.objects.all()
+def show_wishlistxml(request):
+    data = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data,
+    'nama': 'Natasya Ashil Zhafirah'
+    }
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_wishlistjson(request):
+    data = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data,
+    'nama': 'Natasya Ashil Zhafirah'
+    }
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_wishlistxmljson(request, id):
     data = BarangWishlist.objects.filter(pk=id)
     context = {
     'list_barang': data,
     'nama': 'Natasya Ashil Zhafirah'
     }
-    # return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
     # // Jika JSON
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-    # // Jika XML
-    # return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
